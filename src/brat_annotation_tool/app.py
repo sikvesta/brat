31da0829a2c9a5e32b7de1f0f600df095c0233c5
sikvesta <55361770+sikvesta@users.noreply.github.com>
2023-12-07 17:17:07 +0100

    updated app.py

--- src/brat_annotation_tool/app.py
+++ src/brat_annotation_tool/app.py
@@ -4,6 +4,9 @@ from flask import Flask, Blueprint, redirect
 # Prefix configuration
 root_url_prefix = os.getenv('FLASK_APP_URL_PREFIX', '/')
 
+# path to this file
+my_dir = os.path.dirname(os.path.realpath(__file__))
+
 
 # Create app
 def create_app():
@@ -12,6 +15,11 @@ def create_app():
 
     @root_bp.route("/")
     def hello_world():
+        os.system(f'python3 {my_dir}/standalone.py')
+        return "Hello brat"
+
+    @root_bp.route("/brat")
+    def brat():
         return redirect("http://0.0.0.0:8001")
 
     app.register_blueprint(root_bp, url_prefix=root_url_prefix)
